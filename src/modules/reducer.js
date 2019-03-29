c5e604dbf0fae353dbbeb5edd0c48d530112d778
Ashley Hopkins <ahopkins@yozu.co.uk>
2019-03-29 16:07:37 +0000

    add category input, amend handleSubmit and createItem action

---
+++ src/modules/reducer.js
@@ -0,0 +1,27 @@
+import ACTIONS from "./action";
+import _ from "lodash";
+const defaultState = {
+  items: []
+};
+const todoReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case ACTIONS.Types.CREATE_ITEM: {
+      console.log(action);
+      let item = action.payload.item;
+      let category = action.payload.category;
+      let newItem = { id: state.items.length + 1, description: item, category };
+      let newState = _.cloneDeep(state);
+      newState.items.push(newItem);
+      return newState;
+    }
+    case ACTIONS.Types.DELETE_ITEM: {
+      let newState = _.cloneDeep(state);
+      let index = _.findIndex(newState.items, { id: action.payload });
+      newState.items.splice(index, 1);
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+export default todoReducer;
